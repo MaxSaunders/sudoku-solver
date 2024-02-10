be880ccf9deef83bffaf90db017308f2551ca837
Max Saunders <maxsaunders8794@gmail.com>
2024-02-10 03:43:25 -0500

    solver code and tests

---
+++ main.py
@@ -0,0 +1,87 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check section
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+# Iterates over the 2D array representing a sudoku board
+# to print out a visual representation
+# Parameters: 2D Array of a sudoku board
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - -")
+
+        # print(*bo[0])
+        for j in range(len(bo[i])):
+            if j % 3 == 0 and j != 0:
+                print("| ", end='')
+
+            if j == (len(bo[i]) - 1):
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end='')
+        # print("\n")
+
+# Find an empty cell in the board
+# Parameters: 2D Array of a sudoku board with 0's representing empty cells
+# Returns: (x, y) coordinates of the empty cell
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)
+
+    return None
+
+print_board(board)
+solve(board)
+print("_____________________\n")
+print_board(board)
